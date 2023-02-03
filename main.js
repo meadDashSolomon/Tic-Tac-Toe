b90705e876c29f66e9b257559da983ffb3b5fc90
Mead Solomon <mead93@gmail.com>
2023-02-03 10:05:42 -0600

    added event listeners to game board

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ form.addEventListener("submit", (event) => {
   initializeGame(data);
 });
 
-//factory function. data is a const variable that's an object containing the form info.
+//factory function? data is a const variable that's an object containing the form info.
 const initializeVariables = (data) => {
   data.choice = +data.choice;
   data.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
@@ -26,18 +26,44 @@ const initializeVariables = (data) => {
   data.gameOver = false;
 };
 
-//factory function. data is a const variable that's an object containing the form info.
+const addEventListenersToGameBoard = (data) => {
+  document.querySelectorAll(".box").forEach((box) => {
+    box.addEventListener("click", (event) => {
+      playMove(event.target, data);
+    });
+  });
+};
+
+//factory function? data is a const variable that's an object containing the form info.
 const initializeGame = (data) => {
   //initialize game variables
   initializeVariables(data);
 
-  console.log(data);
-
-  //add event listeners to boxes
+  //add event listeners to gameboard
+  addEventListenersToGameBoard(data);
 };
 
-//attach event listeners (click) to each game box
+const playMove = (box, data) => {
+  //is game over? if so, don't do anything.
+  if (data.gameOver || data.round > 8) {
+    return;
+  }
+  //check if box has a letter, if so, don't do anything
+  if (data.board[box.id] === "X" || data.board[box.id] === "O") {
+    return;
+  }
+  //adjust DOM for player move, then check win conditions
+  data.board[box.id] = data.currentPlayer;
+  box.textContent = data.currentPlayer;
+  box.classList.add(data.currentPlayer === "X" ? "player1" : "player2");
 
+  //increase the round #
+  data.round++;
+
+  //check win conditions
+
+  console.log(box, data);
+};
 //initialize the game
 
 //check which game mode we're playing
